2a459b7cf56c4d0ee3d89ed0fffd1b02973c8541
diwakrZha <4602643+diwakrZha@users.noreply.github.com>
2024-12-19 10:45:08 +0100

    put back the license declaration for Desoutter

--- src/openProtocolSerializer.js
+++ src/openProtocolSerializer.js
@@ -191,10 +191,33 @@ class OpenProtocolSerializer extends Transform {
     buf.write(pad(sizeMessage - 1, 4), 0, 4, encodingOP);
     buf.write(pad(chunk.mid, 4), 4, 4, encodingOP);
 
+    /*
+        Copyright 2021 Jeremy London
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+        */
+
+    /* 
+        Desoutter controllers did not like the version number. 
+        Sending blank characters for this header option got it to work
+        */
+
+    /* 
+        Removes extra serialization code that is not needed by Desoutter controller
+        */
     buf.write("            ", 8, encodingOP);
     buf.write(chunk.payload.toString(encodingOP), 20, encodingOP);
     buf.write("\u0000", sizeMessage, encodingOP);
-
     debug("Bosch/Desoutter serialization result:", buf);
     this.push(buf);
 
